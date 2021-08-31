b6494f86071395967fc0f634b6b1177c44f9b950
erick <muriithierick3758@gmail.com>
2021-08-31 23:23:51 +0300

    used w-auto instead of w-4/5 for the different flex items

---
+++ resources/views/AboutMaseno/brief.blade.php
@@ -0,0 +1,70 @@
+@extends('app')
+@section('content')
+    <div class="  m-auto text-center bg-gray-200  container">
+        <div class="container bg-white ">
+            <div class="flex ">
+                <div class="w-1/3 ml-5  ">
+
+                    <div class="mx-auto border mt-2 box-border w-auto p-2 mb-2">
+                        <h4 class="border-b border-gray-200 text-semibold p-2 bg-gray-50">The Road to Higher Education</h4>
+
+                        <div class=" px-2 py-3  border-t w-4/5">
+                            <img src="#" alt="maseno gate ">
+                        </div>
+                        <div class=" border-t ">
+                            <h4 class="text-semibold p-2">Contact</h4>
+                            <span>Director,Public Relations</span>
+                            <span>Maseno University</span>
+                            <span>Private Bag</span>
+                            <span>Maseno,kenya</span>
+                            <span>Tel:057351040</span>
+                            <span>Fax +254-57-351221</span>
+                            <p>Email:directorpr@maseno.ac.ke</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sed. Ab laboriosam
+                                molestias ex nesciunt architecto nobis facere.
+                                Praesentium repellat aspernatur cum nisi rem est molestiae qui voluptate distinctio
+                                suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique rerum eum
+                                quibusdam. Deleniti tenetur ratione molestiae, corporis ab non,
+                                vel nesciunt quidem soluta dolorem quo aperiam voluptatem consequuntur perspiciatis.</p>
+
+
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Amet libero sed nulla tenetur voluptate facilis ab quidem omnis repudiandae ipsam accusamus,
+                                    velit minus. Omnis doloribus quisquam aut autem nostrum dolore?</p>
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="w-2/3   ml-10">
+                    <div class="bg-gray-200 border p-2  mr-3 mt-2">
+                        <span class="text-left"><strong>You are here:</strong>maseno in Brief</span>
+
+                    </div>
+                    <div class="w-full p-2 h-auto">
+                        <img src="student" alt="student" class="img-style">
+                        <strong class="text-left">Administration Block,Maseno University</strong>
+
+                    </div>
+                    <div class="p-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+                            adipisci! Dignissimos excepturi sed quasi nulla minus dicta quia? Quod sint magni incidunt ipsam
+                            omnis obcaecati cum maxime ducimus ullam amet.</p>
+
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Amet libero sed nulla tenetur voluptate facilis ab quidem omnis repudiandae ipsam accusamus,
+                            velit minus. Omnis doloribus quisquam aut autem nostrum dolore?</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+@endsection
