5e80461d612df237695bdc574b8a79938a9a0ae5
erick <muriithierick3758@gmail.com>
2021-09-01 22:35:12 +0300

    created contact component

---
+++ resources/views/components/contacts.blade.php
@@ -0,0 +1,15 @@
+<div class="bg-gray-200 p-2 shadow-md border border-gray-200">
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+    <h4 class="border-b border-gray-200 text-left ">Contacts </h4>
+    <div class="text-left font-semibold">
+        <span> Director, Public Relations</span><br>
+        <span>Maseno University</span><br>
+        <span>Private Bag</span><br>
+        <span>Maseno, Kenya</span><br>
+        <span>Tel: 057 351040</span><br>
+        <span>Fax: +254 - 57 - 351221</span><br>
+        <a href="" style="color:mediumseagreen">E-mail: directorpr@maseno.ac.ke</a><br>
+
+
+    </div>
+</div>
